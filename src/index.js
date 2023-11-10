412f53e13154442eb95e8b9ceea898ebe15f64cf
Ganpat kakar <kakarganpat@gmail.com>
2023-11-10 06:33:40 -0800

    Update css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-/**
-  This CodeSandbox has been automatically generated using
-  `codesandboxer`. If you're curious how that happened, you can
-  check out our docs here: https://github.com/codesandbox/codesandboxer
-
-  If you experience any struggles with this sandbox, please raise an issue
-  on github. :)
-*/
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
